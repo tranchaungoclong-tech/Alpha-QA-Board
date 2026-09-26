40700cdcb0e376407f9d116ec4953d185ab7bfcd
tranchaungoclong-tech <tranchaungoclong-tech@users.noreply.github.com>
2026-09-26 21:17:45 +0700

    v85: EN / 中文 language switch for the whole board

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "qa-board-v84";
-const PRECACHE = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png"];
+const CACHE = "qa-board-v85";
+const PRECACHE = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./i18n.js"];
 let bellArm = null;
 
 self.addEventListener("install", event => {
